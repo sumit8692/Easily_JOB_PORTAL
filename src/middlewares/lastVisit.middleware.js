d64767223e64171765bf45cceb1dcd5493188e97
Sumit Shah <sumit8692@gmail.com>
2024-08-15 13:03:16 +0530

    commenting this because it's showing error on render

--- src/middlewares/lastVisit.middleware.js
+++ src/middlewares/lastVisit.middleware.js
@@ -1,19 +1,19 @@
-const setLastVisit = (req, res, next) => {
-    if (req.cookies.lastVisit) {
-        const lastVisitDate = new Date(req.cookies.lastVisit);
-        // Format date with 12-hour time and AM/PM
-        const formattedDate = `${lastVisitDate.toLocaleDateString()} ${lastVisitDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
-        res.locals.lastVisit = formattedDate;
-    }
+// const setLastVisit = (req, res, next) => {
+//     if (req.cookies.lastVisit) {
+//         const lastVisitDate = new Date(req.cookies.lastVisit);
+//         // Format date with 12-hour time and AM/PM
+//         const formattedDate = `${lastVisitDate.toLocaleDateString()} ${lastVisitDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
+//         res.locals.lastVisit = formattedDate;
+//     }
 
-    const currentVisitDate = new Date();
-    // Format date with 12-hour time and AM/PM
-    const formattedCurrentDate = `${currentVisitDate.toLocaleDateString()} ${currentVisitDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
-    res.cookie('lastVisit', formattedCurrentDate, {
-        maxAge: 2 * 24 * 60 * 60 * 1000,
-    });
+//     const currentVisitDate = new Date();
+//     // Format date with 12-hour time and AM/PM
+//     const formattedCurrentDate = `${currentVisitDate.toLocaleDateString()} ${currentVisitDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
+//     res.cookie('lastVisit', formattedCurrentDate, {
+//         maxAge: 2 * 24 * 60 * 60 * 1000,
+//     });
 
-    next();
-};
+//     next();
+// };
 
-export default setLastVisit;
+// export default setLastVisit;
